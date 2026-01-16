7ae87507343c5a7af09f35952a286528c92e2592
Moricio <fauzanfadhil28@gmail.com>
2026-01-16 17:49:52 +0700

    splashscreen

---
+++ app/src/main/java/com/example/temankb/Splashscreen.kt
@@ -0,0 +1,20 @@
+package com.example.temankb
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class Splashscreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splashscreen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }, 3000) // 3000 ms = 3 detik
+    }
+}
